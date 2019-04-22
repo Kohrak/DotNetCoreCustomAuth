57ee301de4a5e03a8cafafb6ff4e4160996e385f
Gabriel@Electrika <gabriel@electrika.com>
2019-04-22 21:38:37 +0100

    add basic token auth handler

---
+++ Auth.Tests/TokenAuthHandlerTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace Auth.Tests
+{
+    public class TokenAuthHandlerTests
+    {
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public async void NoKeyGiven_AuthFails(string token)
+        {
+
+            var authResult = await AuthenticateWithToken(token, GetDefaultTokenServiceMock());
+            Assert.NotNull(authResult);
+            Assert.False(authResult.Succeeded);
+            Assert.NotNull(authResult.Failure);
+        }
+
+        [Fact]
+        public async void GivenInvalidKey_AuthFails()
+        {
+            var authResult = await AuthenticateWithToken("invalid", GetDefaultTokenServiceMock());
+            Assert.NotNull(authResult);
+            Assert.False(authResult.Succeeded);
+            Assert.NotNull(authResult.Failure);
+        }
+
+        [Fact]
+        public async void GivenValidKey_AuthenticationPasses()
+        {
+            var authResult = await AuthenticateWithToken("validKey", GetDefaultTokenServiceMock());
+            Assert.True(authResult.Succeeded);
+            Assert.True(authResult.Principal.Identity.IsAuthenticated);
+        }
+
+        private async Task<AuthenticateResult> AuthenticateWithToken(string token, ITokenService tokenService)
+        {
+            var context = new DefaultHttpContext();
+            if (token != null)
+            {
+                context.Request.Headers["AuthToken"] = token;
+            }
+
+            var handler = await InitHandlerAsync(context, tokenService);
+            return await handler.AuthenticateAsync();
+        }
+
+        private TokenAuthHandler GetHandler(ITokenService tokenService)
+        {
+            var options = new Mock<IOptionsMonitor<TokenAuthHandlerOptions>>();
+            var loggerFactory = new Mock<ILoggerFactory>();
+            var logger = new Mock<ILogger>();
+            var encoder = new Mock<UrlEncoder>();
+            var systemClock = new Mock<ISystemClock>();
+
+            loggerFactory.Setup(x => x.CreateLogger(It.IsAny<string>())).Returns(logger.Object);
+
+            return new TokenAuthHandler(options.Object, loggerFactory.Object, encoder.Object, systemClock.Object, tokenService);
+        }
+
+        private async Task<TokenAuthHandler> InitHandlerAsync(HttpContext context, ITokenService tokenService)
+        {
+            var handler = GetHandler(tokenService);
+            await handler.InitializeAsync(
+                new AuthenticationScheme("TokenAuthentication", "TokenAuthentication", handler.GetType()), context);
+            return handler;
+        }
+
+        private ITokenService GetDefaultTokenServiceMock()
+        {
+            var moq = new Mock<ITokenService>();
+            moq.Setup(x => x.IsTokenValid(It.Is<string>(n => n == "validKey"))).Returns(true);
+
+            moq.Setup(x => x.IsTokenValid(It.Is<string>(n => n == null || n != "validKey"))).Returns(false);
+
+            return moq.Object;
+        }
+    }
+}
---
+++ Auth/TokenAuthHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Auth
+{
+    public class TokenAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly ITokenService _tokenService;
+        public TokenAuthHandler(
+            IOptionsMonitor<TokenAuthHandlerOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock,
+            ITokenService tokenService)
+            : base(options, logger, encoder, clock)
+        {
+            _tokenService = tokenService;
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var token = GetTokenFromHeaders("AuthToken");
+            var validationResult = _tokenService.IsTokenValid(token);
+            if (validationResult)
+            {
+                var ticket = GetAuthTicket(null);
+                return await Task.FromResult(AuthenticateResult.Success(ticket));
+            }
+            return await Task.FromResult(AuthenticateResult.Fail("Invalid Token"));
+        }
+
+        private string GetTokenFromHeaders(string tokenName)
+        {
+            return Request.Headers[tokenName];
+        }
+
+        private AuthenticationTicket GetAuthTicket(List<Claim> claims)
+        {
+            var claimList = claims != null && claims.Any() ? claims : new List<Claim>();
+            var identity = new ClaimsIdentity(claimList, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+            return ticket;
+        }
+    }
+
+    public class TokenAuthHandlerOptions : AuthenticationSchemeOptions
+    {
+        //No need for custom options at the moment
+    }
+}
