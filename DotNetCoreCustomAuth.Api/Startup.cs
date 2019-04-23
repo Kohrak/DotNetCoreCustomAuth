45bf4a8c4dc69110e327289a2e88eef7852f583d
Gabriel@Electrika <gabriel@electrika.com>
2019-04-23 21:51:37 +0100

    Add swagger

--- DotNetCoreCustomAuth.Api/Startup.cs
+++ DotNetCoreCustomAuth.Api/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Auth;
 using Microsoft.AspNetCore.Builder;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DotNetCoreCustomAuth.Api
 {
@@ -29,6 +32,25 @@ namespace DotNetCoreCustomAuth.Api
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddAuthentication("CustomTokenAuth").AddCustomTokenAuth();
             services.AddTokenAuthServices();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Custom Auth Api Example", Version = "v1" });
+                c.AddSecurityDefinition(CustomTokenAuth.CustomTokenAuthHeader, new ApiKeyScheme()
+                {
+                    In = "Header",
+                    Description = "Please include the api key provided in the documentation",
+                    Name = CustomTokenAuth.CustomTokenAuthHeader
+                });
+                c.AddSecurityRequirement(
+                    new Dictionary<string, IEnumerable<string>>()
+                    {
+                        { CustomTokenAuth.CustomTokenAuthHeader, Enumerable.Empty<string>() }
+                    }
+                );
+                c.DescribeAllEnumsAsStrings();
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +69,15 @@ namespace DotNetCoreCustomAuth.Api
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.DocumentTitle = "Custom Auth Api Example";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Custom Auth Api Example V1");
+                c.RoutePrefix = string.Empty;
+
+            });
         }
     }
 }
